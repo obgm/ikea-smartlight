83b0169e4d23c2ddee34c64bad8c3b27b5e20edb
Olaf Bergmann <bergmann@tzi.org>
2021-05-12 14:27:12 +0200

    tradfriObject.py: implement method put

--- tradfri/tradfriObject.py
+++ tradfri/tradfriObject.py
@@ -8,15 +8,20 @@ class Tradfri:
         self._tradfriHub = 'coaps://{}:{}'.format(str(self.config.hubip),
                                                   self.config.hubport)
 
-    def cmd(self, path, method="get"):
+    def cmd(self, path, method="get", payload=None):
         uri = 'coaps://{}:{}{}'.format(self.config.hubip,
                                        self.config.hubport,
                                        path)
-        return '{} -m {} -u "{}" -k "{}" "{}"'.format(self.config.coap,
-                                                      method,
-                                                      self.config.identity,
-                                                      self.config.securityid,
-                                                      uri)
+        if payload is not None:
+            e = '-e \'{}\''.format(payload)
+        else:
+            e = ''
+        return '{} -m {} -u "{}" -k "{}" {} "{}"'.format(self.config.coap,
+                                                         method,
+                                                         self.config.identity,
+                                                         self.config.securityid,
+                                                         e,
+                                                         uri)
 
     def get(self, path): 
         if os.path.exists(self.config.coap):
@@ -27,3 +32,11 @@ class Tradfri:
 
         return result.read().strip('\n')
         
+    def put(self, path, payload=None):
+        if os.path.exists(self.config.coap):
+            result = os.popen(self.cmd(path, method="put", payload=payload))
+        else:
+            print("Command not found: " + self.config.coap, file=sys.stderr)
+            sys.exit(1)
+
+        return result.read().strip('\n')
